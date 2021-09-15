78a4ab48e68de8a7235f309c4309d9f63f0b1dfe
JLVilleminot <jeanlouis.villeminot@gmail.com>
2021-09-15 15:21:47 +0200

    fixing typo

--- client/src/widgets/partners/herosection.js
+++ client/src/widgets/partners/herosection.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Clientlogo from '../common/Clientlogo';
+import Clientlogo from '../common/clientlogo';
 
 class Herosection extends Component {
     render() {
