ae8b66b503dd8c55e1cd6f313881c0dd90d4f0a5
JLVilleminot <jeanlouis.villeminot@gmail.com>
2021-12-14 20:53:57 +0100

    Correcting logout route

--- src/routes/authentication/auth.routes.js
+++ src/routes/authentication/auth.routes.js
@@ -5,15 +5,15 @@ const router = express.Router();
 router.get('/auth', function(req, res) {
   if(!req.user){
     req.logout()
-    res.status(200)
+    res.status(200).json()
   }else{
     res.status(200).json(req.user)
   }
 });
 
 router.get('/logout', function(req, res) {
-  req.logout()
-  res.status(200)
+  if(req.user) req.logout()
+  res.status(200).json()
 });
 
 export default router;
