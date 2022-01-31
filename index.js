fafe30abf858d1d2951421193ff1d4722076ca65
JLVilleminot <jeanlouis.villeminot@gmail.com>
2022-02-01 00:14:58 +0100

    Updating cors

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(session({
 }))
 
 app.use(cors({
-  "origin": ["http://localhost:8080", "https://www.silexia.fr"],
+  "origin": ["http://localhost:8080", "https://www.silexia.fr", "http://www.silexia.fr"],
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
   "optionsSuccessStatus": 204,
