a1841c2c845fde7cf81690e7738addc4ea6c47bb
Kimy David <kimy.david.pro@gmail.com>
2022-06-09 22:50:11 +0200

    fix lien partage et heeader sur mobile

--- client/src/layout/header/header_connected.js
+++ client/src/layout/header/header_connected.js
@@ -124,11 +124,11 @@ class Header extends React.Component {
                             nav={false}
                             loop={true}
                         >
-                            { this.state.articles.map((item, i) => 
+                            { this.state.articles.length > 0 ? this.state.articles.map((item, i) => 
                                 <div key={i} className="header-banner-item">
                                     <p><span>Actualités</span> : { item.title } <Link className="link link-primary" to={{pathname: `/blog/${item['id']}`, state: { items: item }}}>Lire l'article</Link></p>
                                 </div>
-                            )}
+                            ) : ''}
                         </OwlCarousel>
                     : '' }
                 </header>
